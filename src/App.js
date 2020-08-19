c828da884f93280c5a3a3fc826ed9339e3aafcb2
marcelopwmendes <marcelopwmendes@gmail.com>
2020-08-19 23:05:52 +0100

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container">
-      <h2>MERN-Stack TODO App</h2>
-    </div>
+    <Router>
+      <div className="container">
+        <h2>MERN-Stack TODO App</h2>
+      </div>
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" exact component={EditTodo} />
+      <Route path="/create" exact component={CreateTodo} />
+    </Router>
   );
 }
 
