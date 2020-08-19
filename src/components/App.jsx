bddc489e2f0015c9a2b228697881fe93d537b25f
marcelopwmendes <marcelopwmendes@gmail.com>
2020-08-19 23:41:56 +0100

    added react components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./CreateTodo";
+import EditTodo from "./EditTodo";
+import TodosList from "./ListTodos";
+
 function App() {
   return (
     <Router>
       <div className="container">
         <h2>MERN-Stack TODO App</h2>
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" exact component={EditTodo} />
+        <Route path="/create" exact component={CreateTodo} />
       </div>
-      <Route path="/" exact component={TodosList} />
-      <Route path="/edit/:id" exact component={EditTodo} />
-      <Route path="/create" exact component={CreateTodo} />
     </Router>
   );
 }
---
+++ src/components/ListTodos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ListTodos() {
+    return (
+        < div >
+            <p>Welcome to Todos List Component !</p>
+        </div >
+    );
+}
+
+export default ListTodos;
