86a793d2934b5f0b541c9ac9cb813c978a96068d
marcelopwmendes <marcelopwmendes@gmail.com>
2020-08-20 00:50:39 +0100

    added navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from "./Navbar";
 import CreateTodo from "./CreateTodo";
 import EditTodo from "./EditTodo";
 import TodosList from "./ListTodos";
@@ -10,7 +11,8 @@ function App() {
   return (
     <Router>
       <div className="container">
-        <h2>MERN-Stack TODO App</h2>
+        <Navbar/>
+
         <Route path="/" exact component={TodosList} />
         <Route path="/edit/:id" exact component={EditTodo} />
         <Route path="/create" exact component={CreateTodo} />
